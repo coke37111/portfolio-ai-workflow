344ea476ebb7f597bb26e1e9435b3e82cda6c2e8
DopaminePlus <dpm.plus.inc@gmail.com>
2026-04-26 15:53:54 +0900

    포트폴리오 업데이트

--- Ai 워크 플로우 포트폴리오/deploy.bat
+++ Ai 워크 플로우 포트폴리오/deploy.bat
@@ -1,4 +1,13 @@
 @echo off
 copy /Y "portfolio.html" "..\index.html"
-echo Done.
+echo [1/3] Copied portfolio.html to index.html
+
+cd ..
+git add index.html
+git commit -m "portfolio update"
+echo [2/3] Committed
+
+git push
+echo [3/3] Pushed
+
 pause
